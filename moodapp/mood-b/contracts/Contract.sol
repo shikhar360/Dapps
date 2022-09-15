7711805162d2132270a11984ca34c7419f4ad32d
shikhar360 <sssonishikhar360@gmail.com>
2022-09-16 00:59:07 +0530

    some functions completed

--- moodapp/mood-b/contracts/Contract.sol
+++ moodapp/mood-b/contracts/Contract.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.9;
 
 contract MyContract {
-    constructor() {}
+    address internal owner;
+    string internal mood;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setMood(string memory _mood) external {
+        mood = _mood;
+    }
+
+    function getMood() external view returns (string memory) {
+        return mood;
+    }
 }
--- moodapp/pages/index.js
+++ moodapp/pages/index.js
@@ -1,9 +1,60 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useContract,
+  useContractRead,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+
+import React, { useState } from "react";
 
 export default function Home() {
+  const { contract } = useContract(
+    "0xF31DF4A816F8B5855452fAfb65057ef4D598899F"
+  );
+
+  const { mutateAsync: setMood } = useContractWrite(contract, "setMood");
+  const { data: moodtx } = useContractRead(contract, "getMood");
+
+  const [moodIs, setMoodIs] = useState("");
+  const [input, setInput] = useState();
+
+  async function setMoodTx(val) {
+    try {
+      console.log(val);
+      const moodTx = await setMood(val.toString());
+
+      console.log("Mood setted successfully");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function gettingMood() {
+    try {
+      const getMoodTx = await moodtx();
+      setMoodIs(getMoodTx);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleInput(e) {
+    setInput(e.target.value);
+    console.log(e.target.value);
+  }
+
   return (
     <div className="container">
       <h1>Hello THIRDWEB</h1>
+      <h2>My mood is {moodIs}</h2>
+
+      <input type="text" onChange={handleInput} />
+      <button className="btn" onClick={() => setMoodTx(input.toString())}>
+        Set Mood
+      </button>
+      <button className="" onClick={gettingMood}>
+        GetMood
+      </button>
       <ConnectWallet accentColor="#f213a4" colorMode="light" />
     </div>
   );
