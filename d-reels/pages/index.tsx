ec53ef4a7ab05ce0db861dec04135779fe356a49
shikhar360 <sssonishikhar360@gmail.com>
2022-11-24 14:05:48 +0530

    initial commit

---
+++ d-reels/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div className={` flex items-start justify-center w-full min-h-screen`}>
+     
+    <h1>hello world</h1>
+      
+    </div>
+  )
+}
